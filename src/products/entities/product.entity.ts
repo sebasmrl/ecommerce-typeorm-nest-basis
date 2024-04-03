f18aedbd0d4bf4fb1ef8199e8584a53008181276
Sebastian Morales <davesebastian99@gmail.com>
2024-04-03 12:08:06 -0500

    Save progress

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,50 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',{ unique: true}) //add contrains
+    title:string;
+
+    @Column('float', { default:0 })
+    price:number;
+
+    @Column({ type:'text', nullable:true})
+    description:string;
+
+    @Column({ type:'text', unique:true})
+    slug:string;
+
+    @Column({type:'int', default:0})
+    stock:number;
+
+    @Column('text',{ array:true})
+    sizes:string[];
+
+    @Column({ type:'text'})
+    gender:string;
+
+
+    //tags
+    //images
+
+    
+    @BeforeInsert()
+    checkSlugProperty(){
+        if(!this.slug) this.slug = this.title;
+          
+        this.slug = this.slug
+            .toLowerCase()
+            .replaceAll(' ','_')
+            .replaceAll("'","");
+    }
+
+    @BeforeUpdate()
+    checkUpdateSlugProperty(){
+        
+    }
+
+}
