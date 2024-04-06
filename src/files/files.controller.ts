20a25c65044fdcb1ddc222bb4a7fa5a09a8ade77
Sebastian Morales <davesebastian99@gmail.com>
2024-04-06 15:48:12 -0500

    Add files resource

---
+++ src/files/files.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file-image') ) // permite acceder al archivo especifico
+  uploadProductFile(
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    return file;
+  }
+
+
+}
